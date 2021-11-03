29f7aef7174662a7b84e9c165a96e14021b17da5
raahem <raahemasghar97@gmail.com>
2021-11-03 19:40:24 +0500

    admin lte

--- store-portal/src/App.js
+++ store-portal/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from './components/Sidebar';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Sidebar />
     </div>
   );
