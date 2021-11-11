2b2dfd97b2605eca5f0fa2da6c8fbe10b2547eeb
TheWicher1992 <ysameer.nadeem24@gmail.com>
2021-11-11 14:19:33 +0500

    browser router shifted to App.js

--- store-portal/src/App.js
+++ store-portal/src/App.js
@@ -7,27 +7,29 @@ import AddProducts from './pages/AddProducts'
 import Collections from './pages/Collections';
 import AddCollections from './pages/AddCollections';
 import ProductDetail from './pages/ProductDetail';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import CollectionDetail from './pages/CollectionDetail';
 import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Navbar />
-      <Sidebar />
-      <div className = "content-wrapper">
-        <Switch>
-          <Route path = "/products" component = {Products}/>
-          <Route path = "/addproduct" component = {AddProducts}/>
-          <Route path = "/collections" component = {Collections} />
-          <Route path = "/addcollection" component = {AddCollections} />
-          <Route path = "/productdetail" component = {ProductDetail} />
-          <Route path = "/collectiondetail" component = {CollectionDetail} />
-          <Route path = "/" component = {LandingPage} />
-        </Switch>
+    <Router>
+      <div className="wrapper">
+        <Navbar />
+        <Sidebar />
+        <div className="content-wrapper">
+          <Switch>
+            <Route path="/products" component={Products} />
+            <Route path="/addproduct" component={AddProducts} />
+            <Route path="/collections" component={Collections} />
+            <Route path="/addcollection" component={AddCollections} />
+            <Route path="/productdetail" component={ProductDetail} />
+            <Route path="/collectiondetail" component={CollectionDetail} />
+            <Route path="/" component={LandingPage} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
