e9266d597dbf98f57b32b842f219790cc10ab2ec
TheWicher1992 <ysameer.nadeem24@gmail.com>
2021-11-15 17:56:51 +0500

    virtual host module added

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
+//module imports
 require('dotenv').config()
 const express = require('express')
-const PORT = process.env.PORT || 5000
 const path = require('path')
 const app = express()
 const cors = require('cors')
+const vhost = require('vhost')
+//custom module imports
 const initRoutes = require('./routes/init.routes')
 const connectDb = require('./db/connectDb')
 
+const PORT = process.env.PORT || 5000
+
 app.use(express.json())
+app.use(vhost('*.*.com', (req, res, next) => {
+  console.log(req.vhost[0])
+  console.log(req.vhost[1])
+  next()
+}))
 app.use("/uploads", express.static(path.join(__dirname, 'uploads')))
 
 app.use(cors())
