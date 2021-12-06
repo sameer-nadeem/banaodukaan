cd7a823b59dcec5d6f99b094a47053423ba0418e
TheWicher1992 <sameer.nadeem24@gmail.com>
2021-12-06 14:43:31 +0500

    sidebar changed in ui

---
+++ store-portal/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className="header" id="header">
+      <div className="header_toggle">
+        <i className='bx bx-menu' id="header-toggle"></i>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
--- store-portal/src/components/Sidebar.js
+++ store-portal/src/components/Sidebar.js
@@ -1,211 +1,105 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { Link, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
+  const PRODUCT = 0
+  const [active, setActive] = useState(null)
+  const [
+    productNavigation,
+    setProductNavigation
+  ] =
+    useState({
+      allProducts: false,
+      inventory: false,
+      transfers: false,
+      brands: false,
+      collections: false
+    })
+  const location = useLocation()
+  useEffect(() => {
+    const path = location.pathname.split('/')[1]
+    const subpath = location.pathname.split('/')[2]
+    switch (path) {
+      case "products":
+      case "collections":
+      case "brands":
+        setActive(PRODUCT)
+        if (subpath === undefined)
+          setProductNavigation(p => ({ allProducts: true }))
+        if (path === 'collections')
+          setProductNavigation(p => ({ collections: true }))
+        if (path === 'brands')
+          setProductNavigation(p => ({ brands: true }))
+        break;
+      default:
+        setActive(null)
+        break;
+    }
+  }, [location.pathname])
   return (
-    <aside className="main-sidebar sidebar-dark-primary elevation-4">
-      {/* Brand Logo */}
-      <Link to="/" >
-        <div className="brand-link">
+    <div className="l-navbar" id="nav-bar">
+      <nav className="nav">
+        <div>
 
-          <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
-          <span className="brand-text font-weight-light">Store Name</span>
-        </div>
-      </Link>
-      {/* Sidebar */}
-      <div className="sidebar">
-        {/* Sidebar user panel (optional) */}
-        <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div className="image">
-            <img src="https://avatars.githubusercontent.com/u/73606371?s=400&u=076780db136b05617c6f04bb3477fdcff8d6f0b8&v=4" className="img-circle elevation-2" alt="User" />
-          </div>
-          <div className="info">
-            <Link to="/" >
-              <div className="d-block">Ahmed Ateeq</div>
+          <Link to="#" className="nav_logo">
+            <i className='bx bx-layer nav_logo-icon'></i>
+            <span className="nav_logo-name">Banaodukaan</span>
+          </Link>
+          <div className="nav_list">
+            <Link to="/products" onClick={() => setActive(0)} className={`nav_link ${active === PRODUCT && `highlight`}`}>
+              <i className='bx bx-grid-alt nav_icon'></i>
+              <span className="nav_name">Products</span>
+            </Link>
+            <div id="product-dropdown" style={active === PRODUCT ? { display: "block" } : { display: "none" }}>
+              <Link to="/products" className={`nav_link ${productNavigation.allProducts && `active`} `}>
+                {/* <i className='bx bxs-right-arrow '></i> */}
+                <i className='bx bx-chevron-right nav_icon'></i>
+                <span className="nav_name">All Products</span>
+              </Link>
+              <Link to="/" className="nav_link">
+                <i className='bx bx-chevron-right nav_icon'></i>
+                <span className="nav_name">Inventory</span>
+              </Link>
+              <Link to="/" className="nav_link">
+                <i className='bx bx-chevron-right nav_icon'></i>
+                <span className="nav_name">Transfers</span>
+              </Link>
+              <Link to="/collections" className={`nav_link ${productNavigation.collections && `active`} `}>
+                <i className='bx bx-chevron-right nav_icon'></i>
+                <span className="nav_name">Collections</span>
+              </Link>
+              <Link to="/brands" className={`nav_link ${productNavigation.brands && `active`} `}>
+                <i className='bx bx-chevron-right nav_icon'></i>
+                <span className="nav_name">Brands</span>
+              </Link>
+            </div>
+            <Link to="#" className="nav_link">
+              <i className='bx bx-user nav_icon'></i>
+              <span className="nav_name">Customers</span>
+            </Link> <Link to="#" className="nav_link">
+              <i className='bx bx-message-square-detail nav_icon'>
+              </i>
+              <span className="nav_name">Orders</span>
+            </Link>
+            <Link to="#" className="nav_link">
+              <i className='bx bx-bookmark nav_icon'></i>
+              <span className="nav_name">Analytics</span>
+            </Link>
+            <Link to="#" className="nav_link">
+              <i className='bx bx-folder nav_icon'></i>
+              <span className="nav_name">Discount</span>
+            </Link>
+            <Link to="#" className="nav_link">
+              <span className="nav_name highlight">Sales Channel</span>
+            </Link>
+            <Link to="#" className="nav_link">
+              <i className='bx bx-bar-chart-alt-2 nav_icon'></i>
+              <span className="nav_name">Online Store</span>
             </Link>
           </div>
         </div>
-        {/* SidebarSearch Form */}
-        <div className="form-inline">
-          <div className="input-group" data-widget="sidebar-search">
-            <input className="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search" />
-            <div className="input-group-append">
-              <button className="btn btn-sidebar">
-                <i className="fas fa-search fa-fw" />
-              </button>
-            </div>
-          </div>
-        </div>
-        {/* Sidebar Menu */}
-        <nav className="mt-2">
-          <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            {/* Add icons to the links using the .nav-icon class
-           with font-awesome or any other icon font library */}
-            <li className="nav-item">
-              <div className="nav-link">
-                <i className="nav-icon fas fa-edit" />
-                <p>
-                  Products
-                  <i className="fas fa-angle-left right" />
-                </p>
-              </div>
-              <ul className="nav nav-treeview">
-                <li className="nav-item">
-                  <div className="nav-link">
-                    <Link to="/products" >
-                      <i className="far fa-circle nav-icon" />
-                      <p>All Products</p>
-                    </Link>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <div className="nav-link">
-                    <Link to="/">
-                      <i className="far fa-circle nav-icon" />
-                      <p>Inventory</p>
-                    </Link>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <div className="nav-link">
-                    <Link to="/">
-                      <i className="far fa-circle nav-icon" />
-                      <p>Transfers</p>
-                    </Link>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <div className="nav-link">
-                    <Link to="/collections">
-                      <i className="far fa-circle nav-icon" />
-                      <p>Collections</p>
-                    </Link>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <div className="nav-link">
-                    <Link to="/brands">
-                      <i className="far fa-circle nav-icon" />
-                      <p>Brands</p>
-                    </Link>
-                  </div>
-                </li>
-              </ul>
-            </li>
-
-
-            {/* now for orders */}
-            <li className="nav-item">
-              <div className="nav-link">
-
-                <i className="nav-icon fas fa-edit" />
-                <p>
-                  Orders
-                  <i className="fas fa-angle-left right" />
-                </p>
-
-              </div>
-              <ul className="nav nav-treeview">
-                <li className="nav-item">
-                  <div className="nav-link">
-                    <Link to="/">
-                      <i className="far fa-circle nav-icon" />
-                      <p>All Orders</p>
-                    </Link>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <div className="nav-link">
-                    <Link to="/">
-                      <i className="far fa-circle nav-icon" />
-                      <p>Drafts</p>
-                    </Link>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <div className="nav-link">
-                    <Link to="/">
-                      <i className="far fa-circle nav-icon" />
-                      <p>Abandoned Checkouts</p>
-                    </Link>
-                  </div>
-                </li>
-              </ul>
-            </li>
-
-            <li className="nav-item">
-              <div className="nav-link">
-                <Link to="/">
-                  <i className="nav-icon far fa-image" />
-                  <p>
-                    Customers
-                  </p>
-                </Link>
-              </div>
-            </li>
-
-
-            <li className="nav-item">
-              <div className="nav-link">
-                <i className="nav-icon fas fa-chart-pie" />
-                <p>
-                  Analytics
-                  <i className="right fas fa-angle-left" />
-                </p>
-              </div>
-              <ul className="nav nav-treeview">
-                <li className="nav-item">
-                  <div className="nav-link">
-                    <Link to="/">
-                      <i className="far fa-circle nav-icon" />
-                      <p>Dashboard</p>
-                    </Link>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <div className="nav-link">
-                    <Link to="/">
-                      <i className="far fa-circle nav-icon" />
-                      <p>Reports</p>
-                    </Link>
-                  </div>
-                </li>
-              </ul>
-            </li>
-
-
-            <li className="nav-item">
-              <div className="nav-link">
-                <Link to="/">
-                  <i className="nav-icon far fa-image" />
-                  <p>
-                    Discounts
-                  </p>
-                </Link>
-              </div>
-            </li>
-
-
-
-            <li className="nav-header">Sales Channel</li>
-            <li className="nav-item">
-              <div className="nav-link">
-                <Link to="/">
-                  <i className="nav-icon fas fa-table" />
-                  <p>
-                    Online Store
-                  </p>
-                </Link>
-              </div>
-            </li>
-
-          </ul>
-        </nav>
-        {/* /.sidebar-menu */}
-      </div>
-      {/* /.sidebar */}
-    </aside>
-
+      </nav >
+    </div >
   )
 }
 
