afd3737a43a3d0d663b0ff722a2feaee2e52a06f
TheWicher1992 <sameer.nadeem24@gmail.com>
2022-01-23 19:01:03 +0500

    store title is a domain so validation added

--- accounts/src/components/Forms/AddStoresForm.js
+++ accounts/src/components/Forms/AddStoresForm.js
@@ -2,9 +2,9 @@ import { useState, useMemo, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Alert from "../Alerts/Alert";
-import countryList from 'react-select-country-list'
-import Select from 'react-select'
-
+import countryList from "react-select-country-list";
+import Select from "react-select";
+let validationCancelToken;
 const AddStoresForm = () => {
   //success modal
   const [show, setShow] = useState(false);
@@ -16,19 +16,18 @@ const AddStoresForm = () => {
   const handleShow = () => setShow(true);
   //success modal states end
 
-  const options = useMemo(() => countryList().getData(), [])
+  const options = useMemo(() => countryList().getData(), []);
   const [title, setTitle] = useState("");
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState("");
   const [adress, setAdress] = useState("");
 
   const [city, setCity] = useState("");
   const [postalCode, setPostalCode] = useState(0);
   const [phone, setPhone] = useState(0);
   const [website, setWebsite] = useState("");
-  const [apartment, setApartment] = useState("")
+  const [apartment, setApartment] = useState("");
   const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState('')
-
+  const [lastName, setLastName] = useState("");
 
   const onChangeTitle = (event) => {
     setTitle(event.target.value);
@@ -40,7 +39,7 @@ const AddStoresForm = () => {
     setAdress(event.target.value);
   };
   const onChangeCountry = (value) => {
-    setValue(value)
+    setValue(value);
   };
 
   const onChangePostalCode = (event) => {
@@ -57,19 +56,16 @@ const AddStoresForm = () => {
   };
   const getProfile = async () => {
     try {
-      const res = await axios.get(`/api/merchant/profile`, {
-      });
-      setFirstName(res.data.merchant.firstName)
-      setLastName(res.data.merchant.lastName)
+      const res = await axios.get(`/api/merchant/profile`, {});
+      setFirstName(res.data.merchant.firstName);
+      setLastName(res.data.merchant.lastName);
     } catch (err) {
       console.log(err);
     }
-  }
-
+  };
 
   const addStores = async (event) => {
-
-    event.preventDefault()
+    event.preventDefault();
 
     const data = {
       title: title,
@@ -78,11 +74,10 @@ const AddStoresForm = () => {
       postalCode: postalCode,
       phone: phone,
       website: website,
-      country: value.label
+      country: value.label,
     };
 
-
-    console.log(data)
+    console.log(data);
 
     try {
       await axios.post(`/api/merchant/store`, data, {
@@ -94,7 +89,6 @@ const AddStoresForm = () => {
     } catch (err) {
       console.log(err);
     }
-
   };
 
   useEffect(() => {
@@ -102,6 +96,41 @@ const AddStoresForm = () => {
     getProfile();
   }, []);
 
+  const [titleError, setTitleError] = useState(null);
+  useEffect(() => {
+    const titleRegex = /^((?!-)[a-z0-9-]{0,63}[a-z0-9]\.)+[a-z]{2,63}$/;
+    const validateStoreTitle = async () => {
+      validationCancelToken && validationCancelToken.cancel();
+      validationCancelToken = axios.CancelToken.source();
+
+      const { data } = await axios.get(
+        `/api/merchant/new-store/validate?store=${title}`,
+        { cancelToken: validationCancelToken.token }
+      );
+      if (!data.isValid) {
+        setTitleError("This title is already taken");
+      }
+    };
+
+    const TitleError = () => (
+      <ul>
+        <li>Store title cannot contain spaces</li>
+        <li>Store title cannot start or end with a number</li>
+        <li>Store title cannot start or end with an underscore</li>
+      </ul>
+    );
+
+    if (title !== "") {
+      if (!titleRegex.test(`${title}.banaodukaan.com`)) {
+        setTitleError(<TitleError />);
+      } else {
+        setTitleError(null);
+        validateStoreTitle();
+      }
+    } else {
+      setTitleError(null);
+    }
+  }, [title]);
 
   return (
     <div>
@@ -124,29 +153,60 @@ const AddStoresForm = () => {
               backgroundColor: "white",
             }}
           >
-            <i style={{ cursor: "pointer" }} onClick={() => history('/my-stores')} class="fas mb-5 fa-2x fa-arrow-left"></i>
+            <i
+              style={{ cursor: "pointer" }}
+              onClick={() => history("/my-stores")}
+              class="fas mb-5 fa-2x fa-arrow-left"
+            ></i>
 
             <div>
               <h1 style={{ fontSize: 24, color: "black" }}>Create Store</h1>
             </div>
 
             <div className="mb-3" style={{ paddingTop: 25 }}>
-              <label className="form-label" style={{ color: "black" }}>
-                Title
-              </label>
-              <input
-                className="form-control"
-                style={{ backgroundColor: "white", color: "black" }}
-                onChange={onChangeTitle}
-                required
-              />
+              <div className="row">
+                <div className="col">
+                  <label className="form-label" style={{ color: "black" }}>
+                    Title{" "}
+                  </label>
+                  <input
+                    className="form-control"
+                    style={{ backgroundColor: "white", color: "black" }}
+                    onChange={onChangeTitle}
+                    value={title}
+                    required
+                  />
+                  <span className="text-danger" style={{ fontWeight: "bold" }}>
+                    {titleError && titleError}
+                  </span>
+                </div>
+                <div className="col">
+                  <label className="form-label" style={{ color: "black" }}>
+                    Store's URL
+                  </label>
+                  <input
+                    className="form-control"
+                    style={{
+                      backgroundColor: "white",
+                      color: "#3F8BC5",
+                      fontWeight: "bold",
+                    }}
+                    value={`${title === "" ? "*" : title}.banaodukaan.com`}
+                    disabled
+                  />
+                </div>
+              </div>
             </div>
 
             <div className="mb-3" style={{ paddingTop: 25 }}>
               <label className="form-label" style={{ color: "black" }}>
                 Country/region
               </label>
-              <Select options={options} value={value} onChange={onChangeCountry} />
+              <Select
+                options={options}
+                value={value}
+                onChange={onChangeCountry}
+              />
             </div>
 
             <div className="row">
@@ -159,7 +219,6 @@ const AddStoresForm = () => {
                     className="form-control"
                     value={firstName}
                     style={{ backgroundColor: "white", color: "black" }}
-
                     required
                   />
                 </div>
@@ -173,7 +232,6 @@ const AddStoresForm = () => {
                     className="form-control"
                     value={lastName}
                     style={{ backgroundColor: "white", color: "black" }}
-
                     required
                   />
                 </div>
@@ -213,7 +271,6 @@ const AddStoresForm = () => {
                   </label>
                   <input
                     className="form-control"
-
                     style={{ backgroundColor: "white", color: "black" }}
                     onChange={onChangeCity}
                     required
@@ -269,11 +326,11 @@ const AddStoresForm = () => {
               backgroundColor: "white",
             }}
           >
-
             <button
               className="btn btn-success"
               style={{ width: "25%" }}
               onClick={(e) => addStores(e)}
+              disabled={titleError !== null || title === ""}
             >
               Add Store
             </button>
