be78a784e95a599f17ce180dc33233a0215e27b2
ali hassan <aleehassan1999@gmail.com>
2022-02-18 15:48:52 +0500

    cart UI done

--- store-client/components/Layout.js
+++ store-client/components/Layout.js
@@ -15,7 +15,7 @@ export default function Layout({ children }) {
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
         <Links />
       </Head>
-      <Navbar />
+      <Navbar /> 
       <main className='container'>
         {
           children
