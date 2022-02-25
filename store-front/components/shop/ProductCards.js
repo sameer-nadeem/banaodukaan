fd41cf242637d65ea685fe6c864b9b20b9f85f14
unknown <22100145@lums.edu.pk>
2022-02-25 15:32:17 +0500

    cart doesnt get updated until refresh

--- store-front/components/shop/ProductCards.js
+++ store-front/components/shop/ProductCards.js
@@ -4,6 +4,11 @@ import useURL from "../../utils/useURL";
 import { useRouter } from 'next/router'
 
 const ProductCards = () => {
+    const [cart, setCart] = useState({products:[]});
+    const [isEmpty, setEmpty] = useState(false);
+    const [total, setTotal] = useState(0);
+
+
     const [products, setProducts] = useState([]);
     const url = useURL();
     const router = useRouter();
@@ -13,6 +18,7 @@ const ProductCards = () => {
             pathname: '/product/'+ id,
         })
     }
+    
     const getProducts = async () => {
         try {
             const url = useURL()
@@ -26,6 +32,110 @@ const ProductCards = () => {
     useEffect(() => {
         getProducts();
     }, [])
+
+    useEffect(() => {
+        let cs = localStorage.getItem("cart");
+    
+        if (!cs) {
+          setEmpty(true);
+          return;
+        } else {
+          console.log(cs);
+          cs = JSON.parse(cs)
+          setCart(cs);
+          console.log('cart' , cart)
+    
+          
+        }
+        
+      }, []);
+
+    function addProductToCart(product) {
+
+        console.log("product", product)
+        console.log('we are in add to cart')
+        let cs = '{}'
+        if(typeof window !== 'undefined'){
+          cs = localStorage.getItem("cart");
+        }
+        let cart1;
+        let isAdded = false;
+    
+        if (!cs) {
+          console.log("new cart");
+          cart1 = {
+            products: [
+              {
+                _id: product._id,
+                qty: 1,
+                title: product.title,
+                price: product.price,
+                url: product.image,
+              },
+            ],
+            total : product.price
+          };
+        } else {
+          console.log("cart already present");
+          cart1 = JSON.parse(cs);
+    
+          console.log('cart1', cart1)
+          cart1.products = cart1.products.map((ci) => {
+            if (ci._id == product._id) {
+              console.log("item already present");
+              isAdded = true;
+              return {
+                _id: ci._id,
+                qty: ci.qty + 1,
+                title: ci.title,
+                price: ci.price,
+                url: ci.url,
+              };
+            }
+    
+            return {
+              _id: ci._id,
+              qty: ci.qty,
+              title: ci.title,
+              price: ci.price,
+              url: ci.url,
+            };
+          });
+    
+          if (!isAdded) {
+            console.log("newItem");
+            cart1.products.push({
+              _id: product._id,
+              qty: 1,
+              title: product.title,
+              price: product.price,
+              url: product.image,
+            });
+    
+            if(cart.total === undefined){
+              cart1.total =  product.price
+            }
+            else{
+              cart1.total = cart1.total + product.price
+    
+            }
+          }
+          else{
+            if(cart.total === undefined){
+              cart1.total =  product.price
+            }
+            else{
+              cart1.total = cart1.total + product.price
+    
+            }
+    
+          }
+          
+        }
+        localStorage.setItem("cart", JSON.stringify(cart1));
+        console.log("cart", cart1);
+        
+    }
 
     return (
         <div class="row">
@@ -55,7 +165,7 @@ const ProductCards = () => {
 
                                     <div class="hover-content">
                                         <div class="add-to-cart-btn">
-                                            <a href="#" class="btn essence-btn">Add to Cart</a>
+                                            <a onClick={(e) => addProductToCart(product)} class="btn essence-btn">Add to Cart</a>
                                         </div>
                                     </div>
                                 </div>
