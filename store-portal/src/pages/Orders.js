0683ffc1cbeaf2e21e129d313ab049e9bd236b06
unknown <22100145@lums.edu.pk>
2022-04-01 01:43:17 +0500

    view/update order incomplete

--- store-portal/src/pages/Orders.js
+++ store-portal/src/pages/Orders.js
@@ -7,7 +7,7 @@ const Orders = () => {
       <Card style={{ height: "100%", marginTop: 100, backgroundColor: '#3B8AC4'}}>
         <div style={{ display: "flex" }}>
           <div style={{ flex: 1, justifyContent: "flex-start", padding: 20 }}>
-            <h1 style={{ fontSize: 26, fontWeight: 'bold', color: '#FFFFFF' }}>Products</h1>
+            <h1 style={{ fontSize: 26, fontWeight: 'bold', color: '#FFFFFF' }}>Orders</h1>
           </div>
         </div>
         <OrderTable />
