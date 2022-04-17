935dd1d3d3864cccda0edb37bf5fd63ca04541b7
Haseeb Abid <haseebabid@hotmail.com>
2022-04-17 05:13:58 +0500

    Landing page backend

--- server/controllers/analytics.controller.js
+++ server/controllers/analytics.controller.js
@@ -1,9 +1,9 @@
 const Order = require("../models/order.model")
 const Brand = require("../models/brand.model")
+const { ConnectionStates } = require("mongoose")
 
 const getAnalytics = async (req, res) => {
 
-    console.log("ok")
     const orders = await Order.find({
         storeId: req.storeId
     })
@@ -13,25 +13,19 @@ const getAnalytics = async (req, res) => {
 
     orders.forEach (order =>{
         var products = order.products
-        console.log(products)
         products.forEach(product => {
             let prod = product.product.title;
             let brand = product.product.brandId;
-            console.log(brand)
             if (prod in dict === true) {
-                console.log(1)
                 dict[prod] = dict[prod] + product.qty
             }
             else {
-                console.log(2)
                 dict[prod] = product.qty
             }
             if (brand in dictBrand === true) {
-                console.log(1)
                 dictBrand[brand] = dictBrand[brand] + product.qty
             }
             else {
-                console.log(2)
                 dictBrand[brand] = product.qty
             }
             
@@ -41,19 +35,119 @@ const getAnalytics = async (req, res) => {
         })
     }) 
 
+    const productsSort = Object.entries(dict)
+    .sort(([,a],[,b]) => a-b)
+    .reverse()
+    .splice(0,12)
+    .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+    const sortable = Object.entries(dictBrand)
+    .sort(([,a],[,b]) => a-b)
+    .reverse()
+    .splice(0,12)
+    .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+    console.log(sortable);    
+
+
     dictBrandName ={}
-    for (x in dictBrand) {
+    for (x in sortable) {
         var brandName = await Brand.findOne({_id: x})
-        dictBrandName[brandName.name] = dictBrand[x]
+        dictBrandName[brandName.name] = sortable[x]
     }
-    console.log(dictBrandName)
-    
 
     return res.status(200).json({ordersSales : dict,
                                 BrandSales : dictBrandName
     })
 }
 
+const getSalesbyDate = async (req, res) => {
+    console.log('getSalesbyDate')
+    const sales = await Order.aggregate([
+        {$match: {storeId: req.storeId}},
+        {$group : {_id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
+    sales: {$sum : "$total"}
+    }},
+    {$sort: {_id : -1}},
+    {$limit : 5}
+    ])
+    
+    return res.status(200).json({sales
+})
+
+}
+
+const getOrdersInfo = async (req, res) => {
+    console.log('getSalesbyDate')
+    const orders = await Order.aggregate([
+        {$match: {storeId: req.storeId}},
+        {$group : {_id: "$isDelivered",
+    sales: {$sum : 1}
+    }},
+    ])
+
+    console.log(orders)
+    
+    return res.status(200).json({orders
+})
+
+}
+
+
+const getBrandSales = async (req, res) => {
+
+    const orders = await Order.find({
+        storeId: req.storeId
+    })
+    .populate(['products.product'])
+    var dict = {}
+    var dictBrand = {}
+
+    orders.forEach (order =>{
+        var products = order.products
+        products.forEach(product => {
+            let prod = product.product.title;
+            let brand = product.product.brandId;
+            if (brand in dictBrand === true) {
+                dictBrand[brand] = dictBrand[brand] + product.qty
+            }
+            else {
+                dictBrand[brand] = product.qty
+            }
+            
+
+
+            
+        })
+    }) 
+
+    const sortable = Object.entries(dictBrand)
+    .sort(([,a],[,b]) => a-b)
+    .reverse()
+    .splice(0,3)
+    .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+    console.log(sortable);    
+
+
+    dictBrandName ={}
+    for (x in sortable) {
+        var brandName = await Brand.findOne({_id: x})
+        dictBrandName[brandName.name] = sortable[x]
+    }
+    
+
+    return res.status(200).json({
+                                BrandSales : dictBrandName
+    })
+}
+
+
+
+
 module.exports = {
-    getAnalytics
+    getAnalytics,
+    getSalesbyDate,
+    getOrdersInfo,
+    getBrandSales,
 }
