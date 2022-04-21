9be75e3c98a31d5396239b84bb251406d0561dc7
TheWicher1992 <sameer.nadeem24@gmail.com>
2022-04-22 00:43:01 +0500

    builds v3

--- server/accounts/index.js
+++ server/accounts/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const authRouter = require('./routes/auth.routes')
 const merchantRouter = require("./routes/merchant.routes")
-
+const path = require('path')
 app.use('/api/auth', authRouter)
 app.use('/api/merchant', merchantRouter)
 
