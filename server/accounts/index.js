9e00fcf6b4b09ed346fd339afe88b4b3b2844949
TheWicher1992 <sameer.nadeem24@gmail.com>
2022-04-22 00:41:49 +0500

    builds v2

--- server/accounts/index.js
+++ server/accounts/index.js
@@ -4,7 +4,11 @@ const authRouter = require('./routes/auth.routes')
 const merchantRouter = require("./routes/merchant.routes")
 
 app.use('/api/auth', authRouter)
-app.use('/api/merchant',merchantRouter)
+app.use('/api/merchant', merchantRouter)
 
+app.use('/', express.static(path.join(__dirname, "../../accounts", 'build')))
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, "../../accounts", 'build', 'index.html'));
+});
 
 module.exports = app
